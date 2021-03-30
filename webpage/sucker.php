8cf3d57ec51b5c9781dcf286c6f137739eab374f
Mukhammadsaid <place2play2000@gmail.com>
2021-03-30 22:04:18 +0500

    finished'

--- webpage/sucker.php
+++ webpage/sucker.php
@@ -1,10 +1,12 @@
 <?php
-    $name = $_POST["name"];
-
-    if (isset($_POST["name"])) {
-
+    $is_validated = true;
+    if (isset($_POST["name"]) and isset($_POST["card"]) and isset($_POST["payment"])) {
+        if ($_POST["name"] == "" | $_POST["card"] == "" | $_POST["payment"] == "") {
+            $is_validated = false;
+        }
     }
 
+    $name = $_POST["name"];
     $section = $_POST["section"];
     $card = $_POST["card"];
     $payment = $_POST["payment"];
@@ -21,34 +23,7 @@
     <link href="buyagrade.css" type="text/css" rel="stylesheet" />
 </head>
 
-<body>
-<h1>Thanks, sucker!</h1>
-
-<p>
-Your information has been recorded.
-</p>
-
-    <dl>
-        <dt>Name</dt>
-        <dd>
-            <?= $name ?>
-        </dd>
-
-        <dt>Section</dt>
-        <dd>
-            <?= $section ?>
-        </dd>
-
-        <dt>Credit Card</dt>
-        <dd>
-            <?= "{$card} ({$payment})" ?>
-        </dd>
-    </dl>
-
-    <p>Here are all the suckers who have submitted here:</p>
 <?php
-        $text = file_get_contents("suckers.txt");
-        ?>
-<pre><?= $text ?></pre>
-</body>
-</html>
+$testing = $is_validated ? require('sucker_success.php') : require('sucker_fail.php');
+$testing($name, $section, $card, $payment);
+?></html>
---
+++ webpage/sucker_fail.php
@@ -0,0 +1,6 @@
+<body>
+<h1>Sorry</h1>
+<p>
+    You didn't fill our the form completely. <a href="buyagrade.html">Try again?</a>
+</p>
+</body>
